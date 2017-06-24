1ace6f81e27237c3425c5878a80862b283f40f0c
leonwanghui <wanghui71leon@gmail.com>
2017-06-24 10:58:11 +0800

    Initial commit

---
+++ client/client.go
@@ -0,0 +1,190 @@
+// Copyright (c) 2016 Huawei Technologies Co., Ltd. All Rights Reserved.
+//
+//    Licensed under the Apache License, Version 2.0 (the "License"); you may
+//    not use this file except in compliance with the License. You may obtain
+//    a copy of the License at
+//
+//         http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+//    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+//    License for the specific language governing permissions and limitations
+//    under the License.
+
+/*
+This module implements a standard SouthBound interface of volume resource to
+storage plugins.
+*/
+
+package client
+
+import (
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"net/http"
+	"time"
+
+	"github.com/astaxie/beego/httplib"
+
+	"github.com/kubernetes-incubator/service-catalog/contrib/pkg/api"
+)
+
+const (
+	URL_PREFIX string = "http://192.168.0.9:50040"
+)
+
+func ListProfiles() (*[]api.StorageProfile, error) {
+	url := URL_PREFIX + "/api/v1/profiles"
+
+	// fmt.Println("Start GET request to list profiles, url =", url)
+	req := httplib.Get(url).SetTimeout(100*time.Second, 50*time.Second)
+
+	resp, err := req.Response()
+	if err != nil {
+		return nil, err
+	}
+	err = CheckHTTPResponseStatusCode(resp)
+	if err != nil {
+		return nil, err
+	}
+	rbody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var prfs = &[]api.StorageProfile{}
+	if err = json.Unmarshal(rbody, prfs); err != nil {
+		return nil, err
+	}
+	return prfs, nil
+}
+
+func CreateVolume(name string, size int32) (*api.VolumeResponse, error) {
+	url := URL_PREFIX + "/api/v1/volumes"
+	vr := &api.VolumeRequest{
+		Schema: &api.VolumeOperationSchema{
+			Name: name,
+			Size: size,
+		},
+	}
+
+	// fmt.Println("Start POST request to create volume, url =", url)
+	req := httplib.Post(url).SetTimeout(100*time.Second, 50*time.Second)
+	req.JSONBody(vr)
+
+	resp, err := req.Response()
+	if err != nil {
+		return nil, err
+	}
+	err = CheckHTTPResponseStatusCode(resp)
+	if err != nil {
+		return nil, err
+	}
+	rbody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var vresp = &api.VolumeResponse{}
+	if err = json.Unmarshal(rbody, vresp); err != nil {
+		return nil, err
+	}
+	return vresp, nil
+}
+
+func ListVolumes() (*[]api.VolumeResponse, error) {
+	url := URL_PREFIX + "/api/v1/volumes"
+
+	// fmt.Println("Start GET request to list volumes, url =", url)
+	req := httplib.Get(url).SetTimeout(100*time.Second, 50*time.Second)
+
+	resp, err := req.Response()
+	if err != nil {
+		return &[]api.VolumeResponse{}, err
+	}
+	err = CheckHTTPResponseStatusCode(resp)
+	if err != nil {
+		return &[]api.VolumeResponse{}, err
+	}
+	rbody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return &[]api.VolumeResponse{}, err
+	}
+
+	var vols = &[]api.VolumeResponse{}
+	if err = json.Unmarshal(rbody, vols); err != nil {
+		return &[]api.VolumeResponse{}, err
+	}
+	return vols, nil
+}
+
+func DeleteVolume(volID string) (*api.DefaultResponse, error) {
+	url := URL_PREFIX + "/api/v1/volumes/" + volID
+	vr := &api.VolumeRequest{
+		Schema: &api.VolumeOperationSchema{},
+	}
+
+	// fmt.Println("Start DELETE request to delete volume, url =", url)
+	req := httplib.Delete(url).SetTimeout(100*time.Second, 50*time.Second)
+	req.JSONBody(vr)
+
+	resp, err := req.Response()
+	if err != nil {
+		return nil, err
+	}
+	err = CheckHTTPResponseStatusCode(resp)
+	if err != nil {
+		return nil, err
+	}
+	rbody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var vresp = &api.DefaultResponse{}
+	err = json.Unmarshal(rbody, vresp)
+	if err != nil {
+		return nil, err
+	}
+	return vresp, nil
+}
+
+// CheckHTTPResponseStatusCode compares http response header StatusCode against expected
+// statuses. Primary function is to ensure StatusCode is in the 20x (return nil).
+// Ok: 200. Created: 201. Accepted: 202. No Content: 204. Partial Content: 206.
+// Otherwise return error message.
+func CheckHTTPResponseStatusCode(resp *http.Response) error {
+	switch resp.StatusCode {
+	case 200, 201, 202, 204, 206:
+		return nil
+	case 400:
+		return errors.New("Error: response == 400 bad request")
+	case 401:
+		return errors.New("Error: response == 401 unauthorised")
+	case 403:
+		return errors.New("Error: response == 403 forbidden")
+	case 404:
+		return errors.New("Error: response == 404 not found")
+	case 405:
+		return errors.New("Error: response == 405 method not allowed")
+	case 409:
+		return errors.New("Error: response == 409 conflict")
+	case 413:
+		return errors.New("Error: response == 413 over limit")
+	case 415:
+		return errors.New("Error: response == 415 bad media type")
+	case 422:
+		return errors.New("Error: response == 422 unprocessable")
+	case 429:
+		return errors.New("Error: response == 429 too many request")
+	case 500:
+		return errors.New("Error: response == 500 instance fault / server err")
+	case 501:
+		return errors.New("Error: response == 501 not implemented")
+	case 503:
+		return errors.New("Error: response == 503 service unavailable")
+	}
+	return errors.New("Error: unexpected response status code")
+}
---
+++ controller/controller.go
@@ -0,0 +1,168 @@
+/*
+Copyright 2016 The Kubernetes Authors.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controller
+
+import (
+	"encoding/json"
+	"fmt"
+	"sync"
+
+	"github.com/golang/glog"
+	"github.com/kubernetes-incubator/service-catalog/contrib/pkg/broker/controller"
+	"github.com/kubernetes-incubator/service-catalog/pkg/brokerapi"
+	"github.com/leonwanghui/opensds-broker/client"
+)
+
+type errNoSuchInstance struct {
+	instanceID string
+}
+
+func (e errNoSuchInstance) Error() string {
+	return fmt.Sprintf("no such instance with ID %s", e.instanceID)
+}
+
+type openSDSServiceInstance struct {
+	Name       string
+	Credential *brokerapi.Credential
+}
+
+type openSDSController struct {
+	rwMutex     sync.RWMutex
+	instanceMap map[string]*openSDSServiceInstance
+}
+
+// CreateController creates an instance of an OpenSDS service broker controller.
+func CreateController() controller.Controller {
+	var instanceMap = make(map[string]*openSDSServiceInstance)
+	return &openSDSController{
+		instanceMap: instanceMap,
+	}
+}
+
+func (c *openSDSController) Catalog() (*brokerapi.Catalog, error) {
+	prfs, err := client.ListProfiles()
+	if err != nil {
+		return &brokerapi.Catalog{}, err
+	}
+
+	var plans = []brokerapi.ServicePlan{}
+	for _, prf := range *prfs {
+		plan := brokerapi.ServicePlan{
+			Name:        prf.Name,
+			ID:          prf.Id,
+			Description: prf.Description,
+			Metadata:    prf.StorageTags,
+			Free:        true,
+		}
+		plans = append(plans, plan)
+	}
+	return &brokerapi.Catalog{
+		Services: []*brokerapi.Service{
+			{
+				Name:        "opensds-service",
+				ID:          "4f6e6cf6-ffdd-425f-a2c7-3c9258ad2468",
+				Description: "Policy based storage service",
+				Plans:       plans,
+				Bindable:    true,
+			},
+		},
+	}, nil
+}
+
+func (c *openSDSController) CreateServiceInstance(
+	id string,
+	req *brokerapi.CreateServiceInstanceRequest,
+) (*brokerapi.CreateServiceInstanceResponse, error) {
+	capInterface, ok := req.Parameters["capacity"]
+	c.rwMutex.Lock()
+	defer c.rwMutex.Unlock()
+	if ok {
+		capacity := capInterface.(float64)
+		vol, err := client.CreateVolume(id, int32(capacity))
+		if err != nil {
+			return &brokerapi.CreateServiceInstanceResponse{}, err
+		}
+		c.instanceMap[id] = &openSDSServiceInstance{
+			Name: id,
+			Credential: &brokerapi.Credential{
+				"volumeId": vol.Id,
+			},
+		}
+	} else {
+		return &brokerapi.CreateServiceInstanceResponse{}, fmt.Errorf("Capacity not provided in request!")
+	}
+
+	glog.Infof("Created User Provided Service Instance:\n%v\n", c.instanceMap[id])
+	return &brokerapi.CreateServiceInstanceResponse{}, nil
+}
+
+func (c *openSDSController) GetServiceInstance(id string) (string, error) {
+	c.rwMutex.Lock()
+	defer c.rwMutex.Unlock()
+
+	instance, ok := c.instanceMap[id]
+	if ok {
+		body, err := json.Marshal(instance)
+		if err != nil {
+			return "", err
+		}
+		return string(body), nil
+	}
+
+	return "", fmt.Errorf("Can not find instance id %v", id)
+}
+
+func (c *openSDSController) RemoveServiceInstance(id string) (*brokerapi.DeleteServiceInstanceResponse, error) {
+	c.rwMutex.Lock()
+	defer c.rwMutex.Unlock()
+
+	instance, ok := c.instanceMap[id]
+	if ok {
+		volInterface, ok := (*instance.Credential)["volumeId"]
+		if !ok {
+			return &brokerapi.DeleteServiceInstanceResponse{}, fmt.Errorf("Volume id not provided in credential info!")
+		}
+		volID := volInterface.(string)
+		resp, err := client.DeleteVolume(volID)
+		if err != nil {
+			return &brokerapi.DeleteServiceInstanceResponse{}, err
+		} else if resp.Status != "Success" {
+			return &brokerapi.DeleteServiceInstanceResponse{}, fmt.Errorf(resp.Error)
+		}
+		delete(c.instanceMap, id)
+		return &brokerapi.DeleteServiceInstanceResponse{}, nil
+	}
+
+	return &brokerapi.DeleteServiceInstanceResponse{}, nil
+}
+
+func (c *openSDSController) Bind(
+	instanceID,
+	bindingID string,
+	req *brokerapi.BindingRequest,
+) (*brokerapi.CreateServiceBindingResponse, error) {
+	c.rwMutex.RLock()
+	defer c.rwMutex.RUnlock()
+	instance, ok := c.instanceMap[instanceID]
+	if !ok {
+		return nil, errNoSuchInstance{instanceID: instanceID}
+	}
+	cred := instance.Credential
+	return &brokerapi.CreateServiceBindingResponse{Credentials: *cred}, nil
+}
+
+func (c *openSDSController) UnBind(instanceID string, bindingID string) error {
+	// Since we don't persist the binding, there's nothing to do here.
+	return nil
+}
